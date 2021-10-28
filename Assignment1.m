e9ceef75ea7b90a9c54e9d80085029e2a63ab6d5
Hagar814 <93208256+Hagar814@users.noreply.github.com>
2021-10-28 23:27:50 +0200

    Add files via upload

---
+++ Assignment1.m
@@ -0,0 +1,34 @@
+%question 1
+A = [1 2;3 4]
+B = [5 6 ;7 8]
+C =[11 12;13 14]
+D = [15 16;17 18]
+3*A-5*C
+7*A+2*B
+C*A
+C*D'
+
+%question 2
+x = zeros(4)
+x = zeros(3,4)
+x = ones(4) 
+x = ones(3,4)
+size D
+x = zeros (size(D))
+x = diag([1 2 3 4])
+eye(5) 
+
+%question 3
+[A,B]
+[A;B]
+
+%question4
+y = zeros(7,8)
+y =diag([5 5 5 5 5 5 5 5])
+y(8,:)=5
+
+%question5
+A(:,1)
+A(:,2)
+A(1,:)
+A(2,:)
